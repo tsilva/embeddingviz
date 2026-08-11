f3c3f989f32969598b0a70c65c7ab81b23593afe
Tiago Silva <eng.tiago.silva@gmail.com>
2026-08-11 18:02:40 +0100

    chore: optimize Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import { Sentry } from "./sentry";
 import "./styles.css";
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")!, {
 }).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
